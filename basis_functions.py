be0b3c0c31e6fb520ad9e92e2bb4d5ef291eedd8
flabowski <florian.arbes@t-online.de>
2022-09-16 14:22:35 +0200

    added summary

---
+++ basis_functions.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 14:24:29 2022
+
+@author: florianma
+"""
+import numpy as np
+from scipy.linalg import svd, orth, qr
+import matplotlib.pyplot as plt
+
+
+def normalize(U):
+    U_n = U / np.sum(U**2, axis=0)**.5
+    return U_n
+
+
+def rms(difference):
+    L2 = np.mean(difference**2, axis=0)**.5
+    return L2
+
+
+def projection_error(X, basis):
+    difference = X - (basis @ basis.T) @ X
+    return rms(difference)
+
+
+class Basis:
+    def __init__(self):
+        return
+
+    def reduced_basis(self, rank):
+        # truncation
+        return self.U[:, :rank]
+
+    def calc_error(self, X_test, r_max=None):
+        M, N = X_test.shape
+        if not r_max:
+            r_max = N if N < M else M
+        rms_error = np.zeros((r_max,))
+        max_error = np.zeros((r_max,))
+        for r in range(1, r_max//1, r_max//100):
+            U_r = self.reduced_basis(rank=r)
+            L2 = projection_error(X_test, U_r)
+            rms_error[r] = np.sqrt(np.mean(L2**2))
+            max_error[r] = L2.max()
+        return rms_error, max_error
+
+
+class SVD(Basis):
+    name = "svd"
+
+    def __init__(self, X):
+        # based on snapshots X
+        U, S, VT = np.linalg.svd(X, full_matrices=False)
+        is_flipped = VT[:, 1] < 0
+        U[:, is_flipped] *= -1
+        VT[is_flipped, :] *= -1
+        self.U = U
+        self.S = S
+        self.VT = VT
+        return None
+
+
+class Trigonometric(Basis):
+    name = "trigonometric"
+
+    def __init__(self, domain, fun=np.sin, r=None):
+        # based on analytic sine functions
+        m = domain.size
+        if not r:
+            r = m
+        U = np.zeros((m, r))
+        T = domain.max-domain.min
+        delta_x = domain.delta_x
+        A = 2*delta_x**0.5 * np.sin(np.pi/4)
+        x = domain()
+        for i in range(r):
+            omega = 2 * np.pi/(4*T) * (2*i+1)
+            U[:, i] = A * fun(omega*x)
+        self.U = normalize(U)
+        return
+
+
+class Greedy(Basis):
+    name = "greedy"
+
+    def __init__(self, X, r=None):
+        # based on snapshots X
+        print("generating greedy basis. rank: ")
+        m, n = X.shape
+        if not r:
+            r = min(m, n)
+        U = np.zeros_like(X[:, 0, None])
+        err = np.zeros(r,)
+        for i in range(r):
+            print(i, end=", ")
+            psi_i, err[i] = self.next_basis_vec(X, U)
+            U = np.c_[U, psi_i]
+        print()
+        self.U = U
+        return
+
+    def next_basis_vec(self, X, U):
+        error = X - U @ (U.T @ X)
+        L2 = np.mean(error**2, axis=0)**.5
+        worst = np.argmax(L2)
+        return normalize(error[:, worst]), L2[worst]
+
+
+class LNA(Basis):
+    name = "local neighbours average"
+
+    def __init__(self, u, domain):
+        # based on the snapshot generating function u
+        self.u = u
+        self.domain = domain
+
+    def reduced_basis(self, rank):
+        x = self.domain()
+        r = rank
+        m, n = x.size, r+1
+        dm = 1/n
+        mu = np.linspace(dm/2, 1-dm/2, n)
+        X = np.zeros((m, n))  # snapshot matrix
+        for j, mu_j in enumerate(mu):
+            X[:, j] = self.u(x, mu_j)
+        W = (X[:, :-1]+X[:, 1:])/2
+        Q, R = qr(W, mode="economic")
+        self.basis = Q
+        return Q
+
+
+class LPF(Basis):
+    name = "local pulse functions"
+
+    def __init__(self, domain):
+        # based on the snapshot generating function u
+        self.domain = domain
+
+    def reduced_basis(self, rank):
+        x = self.domain()
+        r = rank
+        m = x.size
+        U = np.zeros((m, r))
+        for i in range(r):
+            is_one = (i/r <= x) & (x < (i+1)/r)
+            U[is_one, i] = 1.0
+        assert np.all(np.sum(U, axis=1) == 1), "not orthogonal"
+        return normalize(U)
+
+
+if __name__ == "__main__":
+    from test_functions import Domain, Heaviside
+    plt.close("all")
+    x = Domain([0, 1], 100)
+    mu = Domain([0, 1], 100)
+    u = Heaviside()
+    X = u(x(), mu())
+    svd_basis = SVD(X)
+    trig_basis = Trigonometric(x)
+    greedy_basis = Greedy(X)
+    lna_basis = LNA(u, x)
+    lpf_basis = LPF(x)
+    X_test = X
+    for basis in [svd_basis, trig_basis, greedy_basis, lna_basis, lpf_basis]:
+        basis.calc_error(X_test)
---
+++ initial_conditions.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 14:49:18 2022
+
+@author: florianma
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Domain:
+    def __init__(self, bounds, size):
+        self.bounds = bounds
+        self.size = size
+        self.min, self.max = bounds[0], bounds[1]
+        T = self.max - self.min
+        self.delta_x = dx = T/size
+        self.x = np.linspace(self.min+dx/2, self.max-dx/2, size)
+        return
+
+    def __call__(self):
+        return self.x
+
+
+class Function:
+    name = None
+
+    def __init__(self):
+        return
+
+    def __call__(self, x, mu):
+        if isinstance(mu, float):
+            return self.u(x, mu)
+        m, n = x.size, mu.size
+        X = np.zeros((m, n))  # snapshot matrix
+        for j, mu_j in enumerate(mu):
+            X[:, j] = self.u(x, mu_j)
+        return X
+
+    def u(self, x, mu):
+        return
+
+    def plot(self, x, ax=None, **kwargs):
+        print(kwargs)
+        test_sample = x.size//2
+        mu = x[test_sample]
+        print(mu)
+        if not ax:
+            fig, ax = plt.subplots()
+        ax.plot(x, self.u(x, mu), ".", **kwargs)
+        ax.set_xlabel("x")
+        ax.set_ylabel("u(x; mu={:.4f})".format(mu))
+        if not ax:
+            plt.show()
+        return
+
+
+class Heaviside(Function):
+    name = "heaviside"
+
+    def u(self, x, mu):
+        y = np.zeros_like(x)
+        y[x < mu] = 0.0
+        y[x == mu] = .5
+        y[x > mu] = 1.0
+        return y
+
+
+class Ramp(Function):
+    name = "ramp"
+
+    def __init__(self, epsilon):
+        self.eps = epsilon
+        return
+
+    def u(self, x, mu):
+        eps = self.eps
+        y = .5+1/(2*eps)*(x-mu)
+        y[y < 0] = 0
+        y[y > 1] = 1
+        return y
+
+
+class SmoothJump(Function):
+    name = "smooth jump"
+
+    def __init__(self, epsilon):
+        self.eps = epsilon
+        return
+
+    def u(self, x, mu):
+        epsilon = self.eps
+        y = np.zeros_like(x)
+        intervall1 = x <= (mu-epsilon)
+        intervall2 = ((mu-epsilon) <= x) & (x <= mu)
+        intervall3 = (mu <= x) & (x <= (mu+epsilon))
+        intervall4 = (mu+epsilon) <= x
+        y[intervall1] = 0
+        y[intervall2] = ((x[intervall2]-(mu-epsilon))/epsilon)**2 / 2
+        y[intervall3] = 1-((x[intervall3]-(mu+epsilon))/epsilon)**2 / 2
+        y[intervall4] = 1
+        return y
+
+
+class Sigmoid(Function):
+    name = "sigmoid"
+
+    def __init__(self, a):
+        self.a = a
+        return
+
+    def u(self, x, mu):
+        a = self.a
+        return 1.0 / (1+np.e**(-(x-mu)*a))
+
+
+if __name__ == "__main__":
+    plt.close("all")
+    x = Domain([0, 1], 100)
+    mu = Domain([0, 1], 100)
+    u1 = Heaviside()
+    u2 = Ramp(0.025)
+    u3 = SmoothJump(0.025)
+    u4 = Sigmoid(50)
+    for u in [u1, u2, u3, u4]:
+        print(u.name)
+        u.plot(x())
+        X = u(x(), mu())
+        fig, ax = plt.subplots()
+        ax.imshow(X, interpolation="nearest")
+        plt.show()
